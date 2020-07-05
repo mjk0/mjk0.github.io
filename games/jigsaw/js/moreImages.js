735f6f7ce9aebaf1ddec5e6a873b9c3c703d48fb
mjk0 <53922869+mjk0@users.noreply.github.com>
2020-07-04 23:57:19 -0700

    Update moreImages.js

--- games/jigsaw/js/moreImages.js
+++ games/jigsaw/js/moreImages.js
@@ -7,5 +7,6 @@ js_moreImages = [
     "https://images.unsplash.com/photo-1572298707819-5a59309fc04e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
     "https://images.unsplash.com/photo-1493210977798-4f655ac200a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1162&q=80",
     "https://images.unsplash.com/photo-1488928741225-2aaf732c96cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-    
+    "https://www.fjords.com/wp-content/uploads/photo-gallery/slideshow/wallpaper84.jpg?bwg=1577103120",
+    "https://wallpaperaccess.com/full/247868.jpg",
 ];
